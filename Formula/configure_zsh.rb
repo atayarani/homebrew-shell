b22c563100f475e3776ca74b3ce103fc46e5fb68
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-13 19:42:33 -0700

    fixes

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -1,23 +1,22 @@
 class ConfigureZsh < Formula
-  ver = '0.0.10'
-  zsh_path = File.join('/','usr','local','Cellar','configure_zsh', ver, 'zsh', 'zshrc')
-  desc 'Configure things required by my ZSH setup'
-  homepage 'https://github.com/atayarani/homebrew-shell'
-  url "https://github.com/atayarani/homebrew-shell/archive/v#{ver}.zip"
-  version ver
+  version "0.0.11"
+  desc "Configure things required by my ZSH setup"
+  homepage "https://github.com/atayarani/homebrew-shell"
+  url "https://github.com/atayarani/homebrew-shell/archive/v#{self.version}.tar.gz"
 
-  depends_on 'zplug'
-  depends_on 'zsh'
-  depends_on 'zsh-completions'
-  depends_on 'zsh-syntax-highlighting'
+  depends_on "zplug"
+  depends_on "zsh"
+  depends_on "zsh-completions"
+  depends_on "zsh-syntax-highlighting"
 
   def install
-    bin.install 'true.sh'
-    prefix.install Dir['config/zsh']
-        File.write('/Users/ali/.zshrc', "source #{zsh_path}") unless File.readable?('/Users/ali/.zshrc')
+  zsh_path = File.join("/","usr","local","Cellar","configure_zsh", self.version, "zsh", "zshrc")
+    bin.install "true.sh"
+    prefix.install File.join("config", "zsh")
+        File.write("/Users/ali/.zshrc", "source #{zsh_path}") unless File.readable?("/Users/ali/.zshrc")
   end
 
 test do
-	system 'true'
+	system "true"
 end
 end
