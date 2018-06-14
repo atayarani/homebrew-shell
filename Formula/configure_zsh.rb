9ef1c4c8e73afd705a8c853bd156bb7080b8ebb2
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-13 19:58:47 -0700

    bump

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -1,7 +1,7 @@
 class ConfigureZsh < Formula
   desc "Configure things required by my ZSH setup"
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.13.tar.gz"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.14.tar.gz"
 
   depends_on "zplug"
   depends_on "zsh"
@@ -9,14 +9,16 @@ class ConfigureZsh < Formula
   depends_on "zsh-syntax-highlighting"
 
   def install
-  zsh_link_path = File.join("/","usr","local","Cellar","configure_zsh", version, "zsh", "zshrc")
-  zsh_path = File.join(ENV["HOME"],".zshrc")
+    puts version
+    zsh_link_path = File.join("/","usr","local","Cellar","configure_zsh", version, "zsh", "zshrc")
+    zsh_path = File.join(ENV["HOME"],".zshrc")
     bin.install "true.sh"
     prefix.install File.join("config", "zsh")
-        File.write(zsh_path, "source #{zsh_path}") unless File.readable?(zsh_path)
+    File.write(zsh_path, "source #{zsh_link_path}") unless File.readable?(zsh_path)
   end
 
-test do
-	system "true"
-end
+  test do
+    system "true"
+  end
+
 end
