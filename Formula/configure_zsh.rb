3c905fd577a01837f8032566e73d764c17bf2211
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-13 21:15:17 -0700

    bump

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -2,7 +2,7 @@ require 'pry'
 class ConfigureZsh < Formula
   desc "Configure things required by my ZSH setup"
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.22.tar.gz"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.23.tar.gz"
 
   depends_on "zplug"
   depends_on "zsh"
@@ -10,12 +10,18 @@ class ConfigureZsh < Formula
   depends_on "zsh-syntax-highlighting"
 
   def install
-    zsh_link_path = File.join(prefix, "configure", "zsh", "zshrc")
-    binding.pry
-    zsh_path = File.join(ENV["CURL_HOME"], ".zshrc")
     prefix.install File.join("config", "zsh")
-    File.write(zsh_path, "source #{zsh_link_path}\n") unless File.readable?(zsh_path)
     bin.install "true.sh"
+  end
+
+  def caveats; <<~ EOS
+    In order to load the zshrc here, please add the following to your .zshrc:
+      source #{zsh_link_path}
+  EOS
+  end
+
+  def zsh_link_path
+    @zsh_link_path ||= File.join(prefix, "configure", "zsh", "zshrc")
   end
 
   test do
