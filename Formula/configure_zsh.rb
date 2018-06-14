10bd1072d4ebef6acc54010c37fe6a1bee894a1f
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-13 21:29:21 -0700

    bump

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -2,7 +2,7 @@ require 'pry'
 class ConfigureZsh < Formula
   desc "Configure things required by my ZSH setup"
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.26.tar.gz"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.27.tar.gz"
 
   depends_on "zplug"
   depends_on "zsh"
@@ -14,7 +14,7 @@ class ConfigureZsh < Formula
     bin.install "true.sh"
   end
 
-  def caveats; <<~EOS
+  def caveats; binding.pry; <<~EOS
     In order to load the zshrc here, please run the following:
       echo "source #{File.join(prefix, "zsh", "zshrc")}" > #{ENV["HOME"]}/.zshrc
   EOS
