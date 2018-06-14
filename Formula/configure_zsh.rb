5176c7109106262c133e083610305a29fa782706
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-13 20:33:21 -0700

    bump

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -1,7 +1,8 @@
+require 'pry'
 class ConfigureZsh < Formula
   desc "Configure things required by my ZSH setup"
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.18.tar.gz"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.19.tar.gz"
 
   depends_on "zplug"
   depends_on "zsh"
@@ -11,6 +12,7 @@ class ConfigureZsh < Formula
   def install
     zsh_link_path = File.join(prefix, "configure", "zsh", "zshrc")
     zsh_path = File.join(ENV["HOME"], ".zshrc")
+    binding.pry
     prefix.install File.join("config", "zsh")
     File.write(zsh_path, "source #{zsh_link_path}") unless File.readable?(zsh_path)
     bin.install "true.sh"
