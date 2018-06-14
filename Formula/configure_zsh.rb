eeff51946fa481e624644d942501bc5cfb479a8a
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-13 19:15:03 -0700

    add zshrc

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -1,13 +1,17 @@
 class ConfigureZsh < Formula
   desc 'Configure things required by my ZSH setup'
   homepage 'https://github.com/atayarani/homebrew-shell'
-  url 'https://github.com/atayarani/homebrew-shell/archive/v0.0.5.zip'
-  version '0.0.5'
+  url 'https://github.com/atayarani/homebrew-shell/archive/v0.0.7.zip'
+  version '0.0.7'
 
   depends_on 'zplug'
+  depends_on 'zsh'
+  depends_on 'zsh-completions'
+  depends_on 'zsh-syntax-highlighting'
 
   def install
     bin.install 'true.sh'
+    prefix.install Dir['config/zsh']
   end
 
 test do
