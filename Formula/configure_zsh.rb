78876cc957ebb381e27a73f54241c71377d94ee8
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-13 19:29:28 -0700

    write zshrc

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -1,8 +1,10 @@
 class ConfigureZsh < Formula
+  ver = 'v0.0.9'
+  zsh_path = File.join('/','usr','local','Cellar','configure_zsh', ver.tr('v',''), 'zsh', 'zshrc')
   desc 'Configure things required by my ZSH setup'
   homepage 'https://github.com/atayarani/homebrew-shell'
-  url 'https://github.com/atayarani/homebrew-shell/archive/v0.0.8.zip'
-  version '0.0.8'
+  url "https://github.com/atayarani/homebrew-shell/archive/#{ver}.zip"
+  version ver
 
   depends_on 'zplug'
   depends_on 'zsh'
@@ -12,6 +14,7 @@ class ConfigureZsh < Formula
   def install
     bin.install 'true.sh'
     prefix.install Dir['config/zsh']
+        File.write('/Users/ali/.zshrc', "source #{zsh_path}") unless File.readable?('/Users/ali/.zshrc')
   end
 
 test do
