00ee10b000c5db5f3319f41b8494a32ac35c0e9a
Ali Tayarani <atayarani@users.noreply.github.com>
2018-06-25 10:08:26 -0700

    add rbenv

--- Formula/configure_zsh.rb
+++ Formula/configure_zsh.rb
@@ -1,7 +1,7 @@
 class ConfigureZsh < Formula
   desc "Configure things required by my ZSH setup"
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.41.tar.gz"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.42.tar.gz"
 
   depends_on "zplug"
   depends_on "zsh"
