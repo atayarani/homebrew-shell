c2422b75f87baaee24e7be4f9dc69fb836b61500
Ali Tayarani <atayarani@users.noreply.github.com>
2018-11-03 11:47:57 -0700

    expand brewfile

--- Formula/configure_brewfile.rb
+++ Formula/configure_brewfile.rb
@@ -1,7 +1,7 @@
 class ConfigureBrewfile < Formula
   desc 'Set up of things I used to manually install from a Brewfile'
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.51.tar.gz"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.52.tar.gz"
 
   def install
     prefix.install File.join("config", "brewfile")
