19ed08413dfe3324f83289fae79456b91c159e0d
Ali Tayarani <atayarani@users.noreply.github.com>
2018-11-02 19:42:21 -0700

    Update configure_brewfile.rb

--- Formula/configure_brewfile.rb
+++ Formula/configure_brewfile.rb
@@ -1,9 +1,9 @@
 class ConfigureBrewfile < Formula
   desc 'Set up of things I used to manually install from a Brewfile'
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.47.tar.gz"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.48.tar.gz"
   
   def install
-    system("brew","bundle","--file='#{ENV['HOMEBREW_PREFIX']}/Brewfile'")
+    system("brew","bundle","--file='Brewfile'")
   end
 end
