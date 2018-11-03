912737bbfd59bd6cf22eae85ae09ffa3a4a575ad
Ali Tayarani <atayarani@users.noreply.github.com>
2018-11-02 19:16:41 -0700

    Create configure_brewfile.rb

---
+++ Formula/configure_brewfile.rb
@@ -0,0 +1,9 @@
+class ConfigureBrewfile < Formula
+  desc 'Set up of things I used to manually install from a Brewfile'
+  homepage "https://github.com/atayarani/homebrew-shell"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.43.tar.gz"
+  
+  def install
+    brew bundle --file="#{ENV['HOMEBREW_FIX']/Brewfile"
+  end
+end
