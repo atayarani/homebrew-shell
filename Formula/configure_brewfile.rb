902955dddda4f65c11afa2e76546346ec01bc910
Ali Tayarani <atayarani@users.noreply.github.com>
2018-11-03 11:17:50 -0700

    brewfile update (#1)
    * brewfile update
    
    * brewfile update

--- Formula/configure_brewfile.rb
+++ Formula/configure_brewfile.rb
@@ -1,9 +1,21 @@
 class ConfigureBrewfile < Formula
   desc 'Set up of things I used to manually install from a Brewfile'
   homepage "https://github.com/atayarani/homebrew-shell"
-  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.49.tar.gz"
-  
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.50.tar.gz"
+
   def install
-    system("brew","bundle")
+    prefix.install File.join("config", "vim")
   end
+
+  def caveats;
+    <<~EOS
+    In order to load the Brewfile, please run the following:
+      brew bundle --file=#{brewfile_path}
+    EOS
+  end
+
+  def brewfile_path
+    @brewfile_path ||= File.join(HOMEBREW_PREFIX, "Cellar", name, version, "brewfile", "Brewfile")
+  end
+
 end
--- Formula/configure_vim.rb
+++ Formula/configure_vim.rb
@@ -1,31 +1,32 @@
 class ConfigureVim < Formula
-    desc "Configure things required by my VIM setup"
-    homepage "https://github.com/atayarani/homebrew-shell"
-    url "https://github.com/atayarani/homebrew-shell/archive/v0.0.38.tar.gz"
+  desc "Configure things required by my VIM setup"
+  homepage "https://github.com/atayarani/homebrew-shell"
+  url "https://github.com/atayarani/homebrew-shell/archive/v0.0.38.tar.gz"
 
-    depends_on "vim"
+  depends_on "vim"
 
-    def install
-      prefix.install File.join("config", "vim")
-    end
+  def install
+    prefix.install File.join("config", "vim")
+  end
 
-    def caveats; <<~EOS
+  def caveats;
+    <<~EOS
     In order to load the vimrc here, please run the following:
       mkdir -p #{ENV['HOME']}/.vim/swap
       ln -sin #{vimrc_path} #{ENV["HOME"]}/.vimrc
       ln -sin #{plug_path} #{ENV["HOME"]}/.vim/plug.vim
     EOS
-    end
+  end
 
-    def vimrc_path
-      @vimrc_path ||= File.join(vim_path, "vimrc")
-    end
+  def vimrc_path
+    @vimrc_path ||= File.join(vim_path, "vimrc")
+  end
 
-    def plug_path
-      @plug_path ||= File.join(vim_path, "plug.vim")
-    end
+  def plug_path
+    @plug_path ||= File.join(vim_path, "plug.vim")
+  end
 
-    def vim_path
-      @vim_path ||= File.join(HOMEBREW_PREFIX, "Cellar", name, version, "vim")
-    end
+  def vim_path
+    @vim_path ||= File.join(HOMEBREW_PREFIX, "Cellar", name, version, "vim")
+  end
 end
